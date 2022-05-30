5b05fd7f6be0f1b3352b70e838d232a805c69787
timothyAgevi <timothyagevi@gmail.com>
2022-05-30 15:41:09 +0300

    update return type of addTask function

--- contract/assembly/Models/TaskManager.ts
+++ contract/assembly/Models/TaskManager.ts
@@ -5,10 +5,10 @@ import { TaskInfo } from "./TaskInfo";
 export class TaskManager{
 tasks:Task[]=[];
 //function to add task
-addTask(title:String):void{
+addTask(title:String):TaskInfo{
    const task =new Task(title);//initialze new task
    const id =this.tasks.push(task);//add task to tasks array,push return index which it has bn push into
-   //return new TaskInfo()
+   return new TaskInfo(task,id)
 }
 //function to remove task
 
