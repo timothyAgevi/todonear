b134568ede9bf3211fb34f081bdce997d0f070ba
timothyAgevi <timothyagevi@gmail.com>
2022-05-30 17:26:46 +0300

    add fuction to show all tasks

--- contract/assembly/Models/TaskManager.ts
+++ contract/assembly/Models/TaskManager.ts
@@ -13,6 +13,17 @@ addTask(title:String):TaskInfo{
    const id =this.tasks.push(task);//add task to tasks array,push return index which it has bn push into
    return new TaskInfo(task,id);
 }
+//function to view creted task
+showTask(taskId:i32):TaskInfo |null{
+    if(taskId >=this.tasks.length)return null;  
+    const task= this.tasks[taskId];
+    return new TaskInfo(task,taskId);
+}
+//function to show all tasks
+showAllTasks():Task[]{
+    return this.tasks;
+}
+
 //function to update status of task
 startTask(taskId:i32):bool{
    //check if task id is available 
