8c05accb7dc00c61830e9f98c9454c0255a65f13
timothyAgevi <timothyagevi@gmail.com>
2022-05-30 21:36:57 +0300

    delete boilercode on main.spec.ts

--- contract/assembly/__tests__/main.spec.ts
+++ contract/assembly/__tests__/main.spec.ts
@@ -1,9 +1,1 @@
-import { setGreeting } from '..'
-import { storage, Context } from 'near-sdk-as'
 
-describe('Greeting ', () => {
-  it('should be set and read', () => {
-    setGreeting('hello world')
-    storage.get<string>(Context.sender)
-  })
-})
