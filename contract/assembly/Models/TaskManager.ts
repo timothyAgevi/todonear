5228473a01ccde90a374b490c88042dd6ad29cfa
timothyAgevi <timothyagevi@gmail.com>
2022-05-30 10:21:06 +0300

    add task to tasks array

--- contract/assembly/Models/TaskManager.ts
+++ contract/assembly/Models/TaskManager.ts
@@ -5,7 +5,8 @@ export class TaskManager{
 tasks:Task[]=[];
 //function to add task
 addTask(title:String):void{
-   const task =new Task(title)
+   const task =new Task(title);//initialze new task
+   this.tasks.push(task);//add task to tasks array
 }
 //function to remove task
 
