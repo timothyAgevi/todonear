f085ce324e4af3688da55673c55af3ff5a411b4a
timothyAgevi <timothyagevi@gmail.com>
2022-05-30 16:01:18 +0300

    add fuction to update task status

--- contract/assembly/Models/TaskManager.ts
+++ contract/assembly/Models/TaskManager.ts
@@ -1,15 +1,23 @@
+import { TaskStatus } from "../utils/enums";
 import { Task } from "./Task";
 import { TaskInfo } from "./TaskInfo";
 
 @nearBindgen
 export class TaskManager{
+//array of tasks
 tasks:Task[]=[];
+
 //function to add task
 addTask(title:String):TaskInfo{
-   const task =new Task(title);//initialze new task
+   const task =new Task(title);//initialze new task object
    const id =this.tasks.push(task);//add task to tasks array,push return index which it has bn push into
-   return new TaskInfo(task,id)
+   return new TaskInfo(task,id);
 }
-//function to remove task
-
+//function to update status of task
+startTask(taskId:i32):bool{
+   //check if task id is available 
+   if(taskId >=this.tasks.length)return false;
+   this.tasks[taskId].status=TaskStatus.ACTIVE;
+   return true;
+}
 }
