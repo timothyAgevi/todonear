431fb544f9d15dc09b6192f823c4c8f04fd771a0
timothyAgevi <timothyagevi@gmail.com>
2022-05-30 19:23:14 +0300

    get wallet adress of contract signer

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -1,6 +1,9 @@
+import { Context } from "near-sdk-as";
+
 @nearBindgen
 export class Contract{
   newTask(title:String):void{
-    
+    //get wallet addres of who signed contract
+    const signer=Context.sender;
   }
 }
